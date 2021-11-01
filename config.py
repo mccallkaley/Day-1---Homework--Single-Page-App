de7d0bf369e8eee850a48885e41ffa245808c650
mccallkaley <mccall.kaley@gmail.com>
2021-10-31 19:49:37 -0600

    css and fixed enviornment

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 class Config():
     REGISTERED_USERS = {
-    'kevinb@codingtemple.com':{"name":"Kevin","password":"abc123"},
-    'alext@codingtemple.com':{"name":"Alex","password":"Colt45"},
-    'joelc@codingtemple.com':{"name":"Joel","password":"MorphinTime"}
+    'mccall.kaley@gmail.com':{"name":"Mccall","password":"abc123"},
+    
     }
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
